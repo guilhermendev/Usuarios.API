bc7c461dfc5fe653d8e837a7b2667757c5719f56
Guilherme Nogueira <guilherme10.nogueira@gmial.com>
2026-03-27 16:42:59 -0300

    adicionado Logger.

--- Usuarios.API/Controllers/UsuariosController.cs
+++ Usuarios.API/Controllers/UsuariosController.cs
@@ -9,32 +9,45 @@ namespace Usuarios.API.Controllers
     public class UsuariosController : ControllerBase
     {
         private readonly IUsuarioService _service;
+        private readonly ILogger<UsuariosController> _logger;
 
-        public UsuariosController(IUsuarioService service)
+        public UsuariosController(IUsuarioService service, ILogger<UsuariosController> logger)
         {
             _service = service;
+            _logger = logger;
         }
+
 
         [HttpPost]
         public IActionResult Criar([FromBody] UsuarioRequestDto dto)
         {
+            _logger.LogInformation("Criando um novo usuário com nome: {Nome}", dto.Nome);
             _service.Criar(dto);
+            _logger.LogInformation("Usuário criado com sucesso: {Nome}", dto.Nome);
             return Ok("Usuário criado com sucesso!");
         }
 
         [HttpGet]
         public IActionResult BuscarTodos()
         {
+            _logger.LogInformation("Buscando todos os usuários.");
             var usuarios = _service.BuscarTodos();
+            _logger.LogInformation("Usuários encontrados com sucesso!");
             return Ok(usuarios);
         }
 
         [HttpGet("{id}")]
         public IActionResult BuscarPorId(int id)
         {
+            _logger.LogInformation("Buscando usuário com ID: {Id}", id);
             var usuario = _service.BuscarPorId(id);
             if (usuario == null)
                 return NotFound("Usuário não encontrado.");
+            {
+                _logger.LogWarning("Usuário com Id {id} não encontrado.", id);
+                return NotFound("Usuário não encontrado.");
+            }
+            _logger.LogInformation("Usuário com Id {id} encontrado com sucesso!", id);
             return Ok(usuario);
         }
 
@@ -42,7 +55,9 @@ namespace Usuarios.API.Controllers
 
         public IActionResult Atualizar(int id, [FromBody] UsuarioRequestDto dto)
         {
-           _service.Atualizar(id, dto);
+            _logger.LogInformation("Atualizando usuário com ID: {Id}", id);
+            _service.Atualizar(id, dto);
+            _logger.LogInformation("Usuário com ID: {Id} atualizado com sucesso!", id);
             return Ok("Usuário atualizado com sucesso!");
         }
 
@@ -50,7 +65,9 @@ namespace Usuarios.API.Controllers
 
         public IActionResult Deletar(int id)
         {
+            _logger.LogInformation("Deletando usuário com ID: {Id}", id);
             _service.Deletar(id);
+            _logger.LogInformation("Usuário com ID: {Id} deletado com sucesso!", id);
             return Ok("Ususario deletado com sucesso!");
         }
     }
